511b84117d1845a83e4a76a97a8a9dfbcf0ab020
Terris Kremer <terris.kremer@gmail.com>
2024-02-06 13:29:58 -0500

    add fonts

--- src/routes/login/index.tsx
+++ src/routes/login/index.tsx
@@ -7,12 +7,25 @@ import {
   Input,
   Text,
 } from "@/lib/ui";
+import logo from "@/assets/images/CMDS_LeafOnly_Logo-128x128px.png";
 
 export function LoginPage() {
   return (
-    <div className="bg-rootBackground flex flex-col h-full w-full min-h-screen items-center justify-center">
+    <div className="flex flex-col h-full w-full min-h-screen items-center justify-center">
       <Card className="w-[360px]">
-        <CardHeader className="border-b">
+        <CardHeader className="border-b flex flex-row items-center">
+          <img
+            src={logo}
+            alt=""
+            style={{
+              boxShadow:
+                "0 14px 28px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.22)",
+              borderRadius: "100%",
+              width: "51px",
+              height: "51px",
+              marginRight: "1rem",
+            }}
+          />
           <Text>Collections Management Database System (C.M.D.S.)</Text>
         </CardHeader>
         <CardContent>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,9 @@ module.exports = {
   ],
   prefix: "",
   theme: {
+    fontFamily: {
+      body: ["Source Sans Pro", "sans-serif"],
+    },
     container: {
       center: true,
       padding: "2rem",
